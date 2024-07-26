b7688efcb878d59809cd0cfb343baa496b638454
2-quantum <prabhatom720@gmail.com>
2024-07-27 05:25:25 +0530

    2

---
+++ 3string_methods.py
@@ -0,0 +1,12 @@
+a = "abcdE"
+
+print(len(a))  # prints the length
+print(a.find("c"))  # find the index of any letter
+print(a.capitalize())  # makes the first word capital
+print(a.upper())  # makes all letter uppercase
+print(a.lower())  # makes all letter lowercase
+print(a.isdigit())  # checks if there is only digits (returns boolean)
+print(a.isalpha())  # checks if there is only alphabets (returns boolean)
+print(a.count("abc"))  # counts the occurrence of any char/string
+print(a.replace("a", "e"))  # replaces the char/string with another char/string
+print(a*2)  # prints the number of times it is told to print (basically multiplication of string)
