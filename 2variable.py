07067fd46cf2df695f914adccdcc39dd59b16f23
2-quantum <prabhatom720@gmail.com>
2024-07-27 02:21:54 +0530

    1

---
+++ 2variable.py
@@ -0,0 +1,22 @@
+a = "om"  # string
+b = 1  # integer
+c = False  # boolean
+d = 2.1  # float
+print(a)
+print(b)
+print(c)
+print(d)
+# type conversion
+e = str(b)
+print(type(e))
+print(type(b))
+
+# multiple assignment
+f, g, h = "abcd", 12, True  # the value of fgh are abcd 12 True respectively
+print(f)
+print(g)
+print(h)
+i = j = k = 10  # all of these have same value
+print(i)
+print(j)
+print(k)
